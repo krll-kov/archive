54ad8c740cee2d4a56691cbe586e16544487f1d4
Kyrylo K. <63228361+krll-kov@users.noreply.github.com>
2026-09-01 01:02:22 +0200

    crc64 wasm support

--- lib/src/util/crc64.dart
+++ lib/src/util/crc64.dart
@@ -1,4 +1,4 @@
-import '_crc64_html.dart' if (dart.library.io) '_crc64_io.dart';
+import '_crc64_io.dart' if (dart.library.html) '_crc64_html.dart';
 
 int getCrc64(List<int> array, [int crc = 0]) => getCrc64_(array, crc);
 
